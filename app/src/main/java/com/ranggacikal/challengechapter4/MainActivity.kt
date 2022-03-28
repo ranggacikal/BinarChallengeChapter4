b6d78ea7aa95679a5489e5a8ba43aa8ce321ca90
Rangga Cikal <ranggacikal2@gmail.com>
2022-03-29 02:37:01 +0700

    Initial commit

---
+++ app/src/main/java/com/ranggacikal/challengechapter4/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.ranggacikal.challengechapter4
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.ranggacikal.challengechapter4.databinding.ActivityMainBinding
+import com.ranggacikal.challengechapter4.viewmodel.MainViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        binding.imgRefresh.setOnClickListener {
+            binding.tvVs.visibility = View.VISIBLE
+            binding.tvPemainMenang.visibility = View.GONE
+            binding.tvComMenang.visibility = View.GONE
+            binding.tvDraw.visibility = View.GONE
+            binding.imgBatuPlayer.setBackgroundColor(Color.WHITE)
+            binding.imgBatuCom.setBackgroundColor(Color.WHITE)
+            binding.imgKertasCom.setBackgroundColor(Color.WHITE)
+            binding.imgGuntingCom.setBackgroundColor(Color.WHITE)
+        }
+
+        binding.imgBatuPlayer.setOnClickListener {
+            binding.imgBatuCom.setBackgroundColor(Color.WHITE)
+            binding.imgGuntingCom.setBackgroundColor(Color.WHITE)
+            binding.imgKertasCom.setBackgroundColor(Color.WHITE)
+            binding.imgBatuPlayer.setBackgroundColor(Color.GREEN)
+            viewModel.validasiHasil(1)
+            when(viewModel.setCom()){
+                1 -> binding.imgBatuCom.setBackgroundColor(Color.GREEN)
+                2 -> binding.imgKertasCom.setBackgroundColor(Color.GREEN)
+                else -> binding.imgGuntingCom.setBackgroundColor(Color.GREEN)
+            }
+            if (viewModel.hasilValidasi() == "win"){
+                binding.tvVs.visibility = View.GONE
+                binding.tvPemainMenang.visibility = View.VISIBLE
+                binding.tvComMenang.visibility = View.GONE
+                binding.tvDraw.visibility = View.GONE
+            }else if (viewModel.hasilValidasi() == "lose"){
+                binding.tvVs.visibility = View.GONE
+                binding.tvPemainMenang.visibility = View.GONE
+                binding.tvComMenang.visibility = View.VISIBLE
+                binding.tvDraw.visibility = View.GONE
+            }else{
+                binding.tvVs.visibility = View.GONE
+                binding.tvPemainMenang.visibility = View.GONE
+                binding.tvComMenang.visibility = View.GONE
+                binding.tvDraw.visibility = View.VISIBLE
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/ranggacikal/challengechapter4/viewmodel/MainViewModel.kt
@@ -0,0 +1,55 @@
+package com.ranggacikal.challengechapter4.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    var pilihanPlayer : String = ""
+    var pilihanCom: String = ""
+    var hasilValidasiPlayer: String = ""
+    var numberCom: Int = 0
+
+    fun validasiHasil(player: Int){
+        when(player){
+            1 -> pilihanPlayer = "batu"
+            2 -> pilihanPlayer = "kertas"
+            3 -> pilihanPlayer = "gunting"
+        }
+
+        val randomNumber = (1..3).random()
+
+        numberCom = randomNumber
+
+        when(randomNumber){
+            1 -> pilihanCom = "batu"
+            2 -> pilihanCom = "kertas"
+            3 -> pilihanCom = "gunting"
+        }
+
+        if (pilihanPlayer == pilihanCom){
+            hasilValidasiPlayer = "draw"
+        }else if (pilihanPlayer == "batu" && pilihanCom == "gunting"){
+            hasilValidasiPlayer = "win"
+        }else if (pilihanPlayer == "kertas" && pilihanCom == "batu"){
+            hasilValidasiPlayer = "win"
+        }else if (pilihanPlayer == "gunting" && pilihanCom == "kertas"){
+            hasilValidasiPlayer = "win"
+        }else if (pilihanPlayer == "gunting" && pilihanCom == "batu"){
+            hasilValidasiPlayer = "lose"
+        }else if (pilihanPlayer == "batu" && pilihanCom == "kertas"){
+            hasilValidasiPlayer = "lose"
+        }else if(pilihanPlayer == "kertas" && pilihanCom == "gunting"){
+            hasilValidasiPlayer = "lose"
+        }
+    }
+
+    fun setCom(): Int {
+        return numberCom
+
+    }
+
+    fun hasilValidasi(): String {
+        return hasilValidasiPlayer
+    }
+
+}
